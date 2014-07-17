36250acdf5cc3275d8003d794a387d0ce0dc1ba1
cranderveldt <djnaphouse@gmail.com>
2014-07-17 08:30:22 -0400

    portfolio is going in with js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var Main = function ($scope) {
-  
+  $scope.portfolio = [
+    {title: 'GABRIEL', description: 'Polished steel desk with solid wood drawer. Custom hand grinding creates a warm silver/bronze color and feels as smooth as silk. 48" wide by 27" deep by 30" high. Custom sizes and finishes available.', images: ['']}
+    , {title: '', description: '', images: ['']}
+  ]
 };
 var app = angular.module('delalain', []);
 app.controller('Main',['$scope', Main]);
