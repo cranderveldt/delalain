dbc7e3e1d05933193465c36a933ca38ba622446b
cranderveldt <djnaphouse@gmail.com>
2014-07-17 08:55:22 -0400

    basic styles for portfolio page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var Main = function ($scope) {
   $scope.portfolio = [
       {
         title: 'GABRIEL', description: 'Polished steel desk with solid wood drawer. Custom hand grinding creates a warm silver/bronze color and feels as smooth as silk. 48" wide by 27" deep by 30" high. Custom sizes and finishes available.', 
-        images: ['']
+        images: ['/img/press-img.png','/img/Table.png']
       }
     , {
         title: 'GABRIELLE', description: 'Polished steel console with solid wood drawer. Custom hand grinding ensures a unique finish that has a warm silver/bronze color and feels as smooth as silk. 48" wide by 12" deep by 32" high. Custom sizes and finishes available.', 
