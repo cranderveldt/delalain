70762d8b7c55b06ec622e8ea3c4bb5419208e6d3
cranderveldt <djnaphouse@gmail.com>
2014-07-22 19:55:02 -0400

    new portfolio items

--- js/script.js
+++ js/script.js
@@ -9,15 +9,15 @@ var Main = function ($scope) {
   ];
   $scope.portfolio = [
       {
-        title: 'GABRIEL', description: 'Polished steel desk with solid wood drawer. Custom hand grinding creates a warm silver/bronze color and feels as smooth as silk. 48" wide by 27" deep by 30" high. Custom sizes and finishes available.', 
+        title: 'Gabriel', description: 'Polished steel desk with solid wood drawer. Custom hand grinding creates a warm silver/bronze color and feels as smooth as silk. 48" wide by 27" deep by 30" high. Custom sizes and finishes available.', 
         images: ['/images/portfolio/gabriel-1.jpg','/images/portfolio/gabriel-2.jpg','/images/portfolio/gabriel-3.jpg','/images/portfolio/gabriel-4.jpg']
       }
     , {
-        title: 'GABRIELLE', description: 'Polished steel console with solid wood drawer. Custom hand grinding ensures a unique finish that has a warm silver/bronze color and feels as smooth as silk. 48" wide by 12" deep by 32" high. Custom sizes and finishes available.', 
+        title: 'Gabrielle', description: 'Polished steel console with solid wood drawer. Custom hand grinding ensures a unique finish that has a warm silver/bronze color and feels as smooth as silk. 48" wide by 12" deep by 32" high. Custom sizes and finishes available.', 
         images: ['/images/portfolio/gabrielle-1.jpg']
       }
     , {
-        title: 'GABRIELLE DEMI-LUNE', description: 'Polished steel Demi-Lune Console. Custom hand grinding ensures a unique finish that has a warm silver/bronze color and feels as smooth as silk. 44" wide by 16" deep by 32" high. Custom sizes and finishes available.', 
+        title: 'Gabrielle Demi-Lune', description: 'Polished steel Demi-Lune Console. Custom hand grinding ensures a unique finish that has a warm silver/bronze color and feels as smooth as silk. 44" wide by 16" deep by 32" high. Custom sizes and finishes available.', 
         images: ['/images/portfolio/gabrielle_demi_lune-1.jpg','/images/portfolio/gabrielle_demi_lune-2.jpg']
       }
     , {
@@ -25,28 +25,52 @@ var Main = function ($scope) {
         images: ['/images/portfolio/marie-1.jpg','/images/portfolio/marie-2.jpg','/images/portfolio/marie-3.jpg']
       }
     , {
-        title: 'BERTRAND', description: 'Hand carved white oak desk with paint finish. The top if the desk and inside the drawer are covered with matching leather. 48" wide by 27" deep by 30" high. Custom sizes and finishes available upon request.', 
+        title: 'Bertrand', description: 'Hand carved white oak desk with paint finish. The top if the desk and inside the drawer are covered with matching leather. 48" wide by 27" deep by 30" high. Custom sizes and finishes available upon request.', 
         images: ['/images/portfolio/bertrand-1.jpg','/images/portfolio/bertrand-2.jpg','/images/portfolio/bertrand-3.jpg']
       }
     , {
-        title: 'LEON', description: 'White oak chest of drawers with a polished steel case. The drawers have and undermount soft close system that works silently and effortlessly. 65" wide by 20" deep by 32.5" high. Custom sizes and finishes available upon request.', 
+        title: 'Leon', description: 'White oak chest of drawers with a polished steel case. The drawers have and undermount soft close system that works silently and effortlessly. 65" wide by 20" deep by 32.5" high. Custom sizes and finishes available upon request.', 
         images: ['/images/portfolio/leon-1.jpg','/images/portfolio/leon-2.jpg','/images/portfolio/leon-3.jpg']
       }
     , {
-        title: 'LILY', description: 'Quartersawn Zebrawood side-table with hand turned legs. The top of this piece has diamond cut veneer and a solid wood drawer with soft close runners. 48" wide by 27" deep by 30" high. Custom sizes and finishes available.', 
+        title: 'Lily', description: 'Quartersawn Zebrawood side-table with hand turned legs. The top of this piece has diamond cut veneer and a solid wood drawer with soft close runners. 48" wide by 27" deep by 30" high. Custom sizes and finishes available.', 
         images: ['/images/portfolio/lily-1.jpg','/images/portfolio/lily-2.jpg','/images/portfolio/lily-3.jpg']
       }
     , {
-        title: 'MASADA', description: 'Walnut day bed with built-in drawers. 83" wide by 42" deep by 38.5" high. Custom sizes and finishes available upon request.', 
+        title: 'Masada', description: 'Walnut day bed with built-in drawers. 83" wide by 42" deep by 38.5" high. Custom sizes and finishes available upon request.', 
         images: ['/images/portfolio/masada.jpg']
       }
     , {
-        title: 'SIMON', description: 'African mahogany desk with raw steel legs and soft-closing drawers. 60" wide by 22" deep and 29" high. Custom sizes and finishes available upon request.', 
+        title: 'Simon', description: 'African mahogany desk with raw steel legs and soft-closing drawers. 60" wide by 22" deep and 29" high. Custom sizes and finishes available upon request.', 
         images: ['/images/portfolio/simon-1.jpg','/images/portfolio/simon-3.jpg','/images/portfolio/simon-4.jpg']
       }
     , {
-        title: 'GASPAR', description: 'Armoir with wenge wood exterior and white oak interior. Blackened steel base and custom steel handles. 46" wide by 24" deep by 88" high. Custom sizes and finishes available.', 
+        title: 'Gaspar', description: 'Armoir with wenge wood exterior and white oak interior. Blackened steel base and custom steel handles. 46" wide by 24" deep by 88" high. Custom sizes and finishes available.', 
         images: ['/images/portfolio/gaspar-1.jpg','/images/portfolio/gaspar-2.jpg','/images/portfolio/gaspar-3.jpg']
+      }
+    , {
+        title: 'Hannah', description: '', 
+        images: ['/images/portfolio/hannah-1.jpg','/images/portfolio/hannah-2.jpg']
+      }
+    , {
+        title: 'Luma', description: '', 
+        images: ['/images/portfolio/luma-1.jpg','/images/portfolio/luma-2.jpg','/images/portfolio/luma-3.jpg','/images/portfolio/luma-4.jpg']
+      }
+    , {
+        title: 'Marie Cecile', description: '', 
+        images: ['/images/portfolio/marie-cecile-1.jpg','/images/portfolio/marie-cecile-2.jpg']
+      }
+    , {
+        title: 'Marie Chest', description: '', 
+        images: ['/images/portfolio/marie-chest-1.jpg','/images/portfolio/marie-chest-2.jpg','/images/portfolio/marie-chest-3.jpg']
+      }
+    , {
+        title: 'Marie Noir', description: '', 
+        images: ['/images/portfolio/marie-noir-1.jpg','/images/portfolio/marie-noir-2.jpg']
+      }
+    , {
+        title: 'Marie Revision', description: '', 
+        images: ['/images/portfolio/marie-revision-1.jpg','/images/portfolio/marie-revision-2.jpg','/images/portfolio/marie-revision-3.jpg']
       }
   ];
   $scope.getPrimaryImage = function(item) {
